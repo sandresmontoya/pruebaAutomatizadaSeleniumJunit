023472f337b96ef478147780f5b0093ea00a12b9
Santiago Montoya <71548558+sandresmontoya@users.noreply.github.com>
2021-10-15 11:41:53 -0500

    Add files via upload

---
+++ src/test/java/com/toptal/tests/ApplyAsDeveloperTest.java
@@ -0,0 +1,62 @@
+package com.toptal.tests;
+
+import com.toptal.webpages.DeveloperApplyPage;
+import com.toptal.webpages.DeveloperPortalPage;
+import com.toptal.webpages.HomePage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ApplyAsDeveloperTest {
+    WebDriver driver;
+
+    @Before
+    public void setup(){
+        //use FF Driver
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void applyAsDeveloper() {
+        //Create object of HomePage Class
+        HomePage home = new HomePage(driver);
+        home.clickOnDeveloperApplyButton();
+
+        //Create object of DeveloperPortalPage
+        DeveloperPortalPage devportal= new DeveloperPortalPage(driver);
+
+        //Check if page is opened
+        Assert.assertTrue(devportal.isPageOpened());
+
+        //Click on Join Toptal
+        devportal.clikOnJoin();
+
+        //Create object of DeveloperApplyPage
+        DeveloperApplyPage applyPage =new DeveloperApplyPage(driver);
+
+        //Check if page is opened
+        Assert.assertTrue(applyPage.isPageOpened());
+
+        //Fill up data
+        applyPage.setDeveloper_email("smontoyadev@toptal.com");
+        applyPage.setDeveloper_full_name("Santiago Montoya Automated Test");
+        applyPage.setDeveloper_password("password123");
+        applyPage.setDeveloper_password_confirmation("password123");
+        applyPage.setDeveloper_skype("automated_test_skype");
+
+        //Click on join
+        //applyPage.clickOnJoin();
+    }
+
+    @After
+    public void close(){
+        driver.close();
+    }
+}
